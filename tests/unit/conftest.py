9d9deae89063b8e3f61ec4a1a38aaeac571c15d7
Jonathan Casey <Jonathan.J.Casey@gmail.com>
2021-12-22 15:49:34 -0500

    Added test for `add()` in `PostgresOrm`.  This entailed:
    - Adding module fixture to create a temp table for testing.
    - Adding a `ModelTest` to use for tests in this module.
    - A session fixture to create the real schema to be used as needed.
    - A double-check in the create db session fixture to deal with dropping not really fully dropping yet.
    - A slight tweak to error message reporting in raised exception.

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -13,6 +13,8 @@ Module Attributes:
 """
 #pylint: disable=protected-access  # Allow for purpose of testing those elements
 
+import time
+
 import pytest
 
 from grand_trade_auto.database import databases
@@ -24,19 +26,62 @@ _TEST_PG_ENV = 'test'
 
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(name='create_test_db', scope='session', autouse=True)
 def fixture_create_test_db():
     """
     Creates the test database to be used for all tests.
+
+    Note that the `create_test_db_schema` fixture relies on this dropping the
+    database when done.  If that ever changes, that fixture may need to be
+    revisited for cleanup items.
 
     This is NOT to be relied upon for any tests marked as alters_db_schema, but
     they should consider that this will create the database.
     """
     test_db = databases._get_database_from_config(_TEST_PG_DB_ID, _TEST_PG_ENV)
     test_db._drop_db() # Ensure cleared to start
+
+    is_dropped = False
+    conn = test_db.connect(False, 'postgres')
+    for _ in range(10):
+        sql_check_name = f'''
+            SELECT COUNT(datname)
+            FROM pg_database
+            WHERE datname=%(database)s
+        '''
+        cursor = conn.cursor()
+        cursor.execute(sql_check_name, {'database': test_db._database})
+        if cursor.rowcount > 0 and cursor.fetchone()[0] == 0:
+            is_dropped = True
+        cursor.close()
+        if is_dropped:
+            break
+        time.sleep(1)
+    conn.close()
+
     test_db.create_db()
     yield
     test_db._drop_db()
+
+
+
+@pytest.fixture(name='create_test_db_schema', scope='session', autouse=True)
+def fixture_create_test_db_schema(
+        create_test_db):                        #pylint: disable=unused-argument
+    """
+    Creates the tables/schema in the database once the database has been
+    initialized.
+
+    This does NOT destroy the tables after, as the `create_test_db` fixture this
+    depends upon will drop the entire database (it also depends on it setting up
+    first, hence it's inclusion as an arg despite not explicitly used in fixture
+    body).
+
+    This is NOT to be relied upon for any tests marked as alters_db_schema, but
+    they should consider that this will create the tables prior to those tests.
+    """
+    test_db = databases._get_database_from_config(_TEST_PG_DB_ID, _TEST_PG_ENV)
+    test_db._orm.create_schemas()
 
 
 
--- tests/unit/model/test_orm_postgres.py
+++ tests/unit/model/test_orm_postgres.py
@@ -8,13 +8,30 @@ with auto-discovery (others may exist, but will not be part of test suite
 directly).
 
 Module Attributes:
-  N/A
+  logger (Logger): Logger for this module (used for testing).
 
 (C) Copyright 2021 Jonathan Casey.  All Rights Reserved Worldwide.
 """
 #pylint: disable=protected-access  # Allow for purpose of testing those elements
 
+import logging
+import re
+import uuid
+
+import psycopg2.errors
 import pytest
+
+from grand_trade_auto.database import databases
+from grand_trade_auto.database import postgres
+from grand_trade_auto.model import model_meta
+from grand_trade_auto.model import orm_meta
+
+from tests.unit import conftest as unit_conftest
+
+
+
+logger = logging.getLogger(__name__)
+
 
 
 
@@ -95,4 +112,139 @@ def test__create_schema_datafeed_src(pg_test_orm):
 
     _test_create_schema(pg_test_orm, pg_test_orm._create_schema_datafeed_src,
             'datafeed_src')
+    pg_test_orm._db._conn.close()
+
+
+
+@pytest.fixture(scope='module', autouse=True)
+def fixture_create_test_table():
+    """
+    Creates a table in the test database for this test.
+
+    This MUST match the TestModel class in this module.
+    """
+    test_db = databases._get_database_from_config(unit_conftest._TEST_PG_DB_ID,
+            unit_conftest._TEST_PG_ENV)
+    conn = test_db.connect(False)
+    sql = '''
+        CREATE TABLE test_orm_postgres (
+            id integer NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            test_name text,
+            str_data text,
+            int_data integer
+        )
+    '''
+    test_db.execute(sql, conn=conn)
+    conn.close()
+
+
+
+class ModelTest(model_meta.Model):
+    """
+    A test model to use for testing within this module.
+
+    This MUST match with the `fixture_create_test_table` in this module.
+    """
+    _table_name = 'test_orm_postgres'
+
+    _columns = (
+        'id',
+        'test_name',
+        'str_data',
+        'int_data',
+    )
+
+    # Don't need the attributes for each column -- not used
+
+
+
+def test_add(monkeypatch, caplog, pg_test_orm):
+    """
+    Tests the `add()` method in `PostgresOrm`.
+    """
+    caplog.set_level(logging.WARNING)
+
+    good_data = {
+        'test_name': 'test_add',
+        'str_data': str(uuid.uuid4()),
+        'int_data': 1,
+    }
+    bad_id = {
+        'id': 2,
+        'test_name': 'test_add',
+        'str_data': str(uuid.uuid4()),
+        'int_data': 2,
+    }
+    bad_col = {
+        'test_name': 'test_add',
+        'str_data': str(uuid.uuid4()),
+        'int_data': 3,
+        'bad_col': 'nonexistent col'
+    }
+    bad_type = {
+        'test_name': 'test_add',
+        'str_data': str(uuid.uuid4()),
+        'int_data': 'four',
+    }
+
+    conn_2 = pg_test_orm._db.connect(False)
+    cursor_2 = pg_test_orm._db.cursor(conn=conn_2)
+    sql_select = 'SELECT * FROM test_orm_postgres'
+
+    pg_test_orm.add(ModelTest, good_data, cursor=cursor_2, close_cursor=False)
+    cursor = pg_test_orm._db.execute(sql_select, cursor=cursor_2,
+            close_cursor=False)
+    assert cursor == cursor_2
+    assert cursor.rowcount == 1
+    cols = [d[0] for d in cursor.description]
+    results = dict(zip(cols, cursor.fetchone()))
+    assert results['str_data'] == good_data['str_data']
+    cursor.close() # Effectively also closes cursor_2
+
+
+    with pytest.raises(
+            psycopg2.errors.GeneratedAlways           #pylint: disable=no-member
+            ) as ex:
+        pg_test_orm.add(ModelTest, bad_id)
+    assert 'cannot insert into column "id"' in str(ex.value)
+
+    with pytest.raises(orm_meta.NonexistentColumnError) as ex:
+        pg_test_orm.add(ModelTest, bad_col)
+    assert "Invalid columns for ModelTest: ['bad_col']" in str(ex.value)
+
+    with pytest.raises(
+            psycopg2.errors.InvalidTextRepresentation #pylint: disable=no-member
+            ) as ex:
+        pg_test_orm.add(ModelTest, bad_type)
+    assert 'invalid input syntax for type integer: "four"' in str(ex.value)
+    # NOTE: The connection could NOT be reused after this exception test for
+    #    the monkeypatch test below, otherwise cleaning up the database after
+    #    all tests would report a user is still connected.  Reason unknown.
+    pg_test_orm._db._conn.close()
+    pg_test_orm._db._conn = None
+
+    def mock_execute(self, command, val_vars=None, cursor=None, commit=True,
+            close_cursor=True, **kwargs):
+        """
+        Simply logs the mogrified SQL statement for inspection.
+        """
+        #pylint: disable=unused-argument
+        cursor = self.connect().cursor()
+        sql = cursor.mogrify(command, val_vars)
+        self.connect().commit()
+        cursor.close()
+        sql_formatted = re.sub(rb'\n\s+', b' ', sql.strip())
+        logger.warning(sql_formatted)
+
+    monkeypatch.setattr(postgres.Postgres, 'execute', mock_execute)
+    caplog.clear()
+    pg_test_orm.add(ModelTest, good_data)
+    assert caplog.record_tuples == [
+        ('tests.unit.model.test_orm_postgres', logging.WARNING,
+            'b"INSERT INTO test_orm_postgres (test_name,str_data,int_data)'
+            + ' VALUES (\'test_add\','
+            + f' \'{str(good_data["str_data"])}\', 1)"')
+    ]
+
+    conn_2.close()
     pg_test_orm._db._conn.close()
