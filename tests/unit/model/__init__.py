946fd666fd6a46b95972f0ab42fa5714c98f9f8b
Jonathan Casey <Jonathan.J.Casey@gmail.com>
2021-12-30 17:07:00 -0500

    Added `test_models` to test all model definitions in a parametrized fashion.  As of now, only test required is one that tests all attributes are as expected.

--- tests/unit/model/__init__.py
+++ tests/unit/model/__init__.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-module-docstring
 __all__ = [
     'test_model_meta',
+    'test_models',
     'test_orm_meta',
     'test_orm_postgres',
 ]
---
+++ tests/unit/model/test_models.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Tests all of the Model subclasses in the grand_trade_auto.model subpackage.
+
+Per [pytest](https://docs.pytest.org/en/reorganize-docs/new-docs/user/naming_conventions.html),
+all tiles, classes, and methods will be prefaced with `test_/Test` to comply
+with auto-discovery (others may exist, but will not be part of test suite
+directly).
+
+Module Attributes:
+  N/A
+
+(C) Copyright 2021 Jonathan Casey.  All Rights Reserved Worldwide.
+"""
+#pylint: disable=protected-access  # Allow for purpose of testing those elements
+
+from abc import ABC
+import inspect
+
+import pytest
+
+from grand_trade_auto.model import datafeed_src
+
+
+
+@pytest.mark.parametrize('model_cls, extra_attrs', [
+    (datafeed_src.DatafeedSrc, []),
+])
+def test_model_attrs(model_cls, extra_attrs):
+    """
+    Tests all the attributes of the provided Models to ensure it is a complete
+    and expected model definition.
+
+    The `extra_attrs` are any attributes added to a subclass of Model that are
+    not in the Model abstract class nor are columns names.
+    """
+    assert model_cls._table_name is not None
+    assert model_cls._columns is not None
+
+    class EmptyAbstract(ABC):            #pylint: disable=too-few-public-methods
+        """
+        Empty abstract class to facilitate getting list of standard attrs.
+        """
+
+    class EmptyClass(EmptyAbstract):     #pylint: disable=too-few-public-methods
+        """
+        Empty class to facilitate getting list of standard attrs.
+        """
+
+    model_attrs = {m[0] for m in inspect.getmembers(model_cls)
+            if not inspect.isroutine(m[1])}
+    std_attrs = {m[0] for m in inspect.getmembers(EmptyClass)
+            if not inspect.isroutine(m[1])}
+    col_attrs = set(model_cls._columns)
+    other_req_attrs = {
+        '_table_name',
+        '_columns',
+    }
+    extra_attrs = set(extra_attrs)
+
+    assert col_attrs.issubset(model_attrs)
+    assert extra_attrs.issubset(model_attrs)
+    assert std_attrs.issubset(model_attrs)
+    assert not model_attrs \
+            - std_attrs - col_attrs - other_req_attrs - extra_attrs
